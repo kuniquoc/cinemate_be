36a8c6115137599a46c4537112b5e07e15224209
kuniquoc <102220122@sv1.dut.udn.vn>
2025-10-22 18:28:39 +0700

    feat: add API Gateway and shared kernel for microservices architecture

---
+++ gateway/src/main/java/com/pbl6/cinemate/gateway/GatewayApplication.java
@@ -0,0 +1,12 @@
+package com.pbl6.cinemate.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
