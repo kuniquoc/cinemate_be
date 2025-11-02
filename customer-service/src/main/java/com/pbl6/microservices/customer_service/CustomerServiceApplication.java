b972736bc792bf6ffe8bdc5242ddad501e3723dd
BaoDev <giabao7112004@gmail.com>
2025-11-02 15:36:18 +0700

    feature/customer-api

--- customer-service/src/main/java/com/pbl6/microservices/customer_service/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/pbl6/microservices/customer_service/CustomerServiceApplication.java
@@ -1,5 +1,7 @@
 package com.pbl6.microservices.customer_service;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
