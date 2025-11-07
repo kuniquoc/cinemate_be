868986bb0b18c2a82943a760879d2101734ea6ee
Lê Xuân Bá Quốc <127231090+kuniquoc@users.noreply.github.com>
2025-11-08 00:17:51 +0700

    feat: Refactor MinIO storage service to include bucket name in method signatures and update configuration for public host (#33)

--- src/auth-service/src/main/java/com/pbl6/cinemate/auth_service/payload/request/PasswordChangingRequest.java
+++ src/auth-service/src/main/java/com/pbl6/cinemate/auth_service/payload/request/PasswordChangingRequest.java
@@ -6,14 +6,12 @@ import com.pbl6.cinemate.auth_service.constant.CommonConstant;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
-@Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class PasswordChangingRequest {
--- src/auth-service/src/main/java/com/pbl6/cinemate/auth_service/security/config/SecurityConfig.java
+++ src/auth-service/src/main/java/com/pbl6/cinemate/auth_service/security/config/SecurityConfig.java
@@ -24,54 +24,54 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final CustomUserDetailsService customUserDetailsService;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtAuthFilter jwtAuthFilter;
-    private final JwtAuthEntryPoint jwtAuthEntryPoint;
+        private final CustomUserDetailsService customUserDetailsService;
+        private final PasswordEncoder passwordEncoder;
+        private final JwtAuthFilter jwtAuthFilter;
+        private final JwtAuthEntryPoint jwtAuthEntryPoint;
 
-    public final String[] PUBLIC_ENDPOINT = {
-            ApiPath.AUTH + "/sign-up",
-            ApiPath.AUTH + "/login",
-            ApiPath.AUTH + "/log-out",
-            ApiPath.AUTH + "/forgot-password",
-            ApiPath.AUTH + "/reset-password",
-            ApiPath.AUTH + "/verify-account",
-            ApiPath.AUTH + "/verify-otp",
-            ApiPath.AUTH + "/refresh-token",
-            ApiPath.AUTH + "/verify-jwt",
-            ApiPath.AUTH + "/verify-email",
-            ApiPath.AUTH + "/verify-token",
-            "/api/v3/api-docs/**",
-            "/swagger-ui/**",
-            "/swagger-ui.html"
-    };
+        public final String[] PUBLIC_ENDPOINT = {
+                        ApiPath.AUTH + "/sign-up",
+                        ApiPath.AUTH + "/login",
+                        ApiPath.AUTH + "/log-out",
+                        ApiPath.AUTH + "/forgot-password",
+                        ApiPath.AUTH + "/reset-password",
+                        ApiPath.AUTH + "/verify-account",
+                        ApiPath.AUTH + "/verify-otp",
+                        ApiPath.AUTH + "/refresh-token",
+                        ApiPath.AUTH + "/verify-jwt",
+                        ApiPath.AUTH + "/verify-email",
+                        ApiPath.AUTH + "/verify-token",
+                        "/api/v3/api-docs/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/actuator/health"
+        };
 
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+        @Bean
+        public AuthenticationManager authenticationManager(
+                        AuthenticationConfiguration authenticationConfiguration) throws Exception {
+                return authenticationConfiguration.getAuthenticationManager();
+        }
 
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
-        return daoAuthenticationProvider;
-    }
+        @Bean
+        public DaoAuthenticationProvider daoAuthenticationProvider() {
+                DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+                daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+                daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+                return daoAuthenticationProvider;
+        }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf(AbstractHttpConfigurer::disable)
-                .exceptionHandling(exception ->
-                        exception.authenticationEntryPoint(jwtAuthEntryPoint))
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(PUBLIC_ENDPOINT).permitAll()
-                        .anyRequest().authenticated()
-                ).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
-    }
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                return http.csrf(AbstractHttpConfigurer::disable)
+                                .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthEntryPoint))
+                                .sessionManagement(session -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .authorizeHttpRequests(auth -> auth
+                                                .requestMatchers(PUBLIC_ENDPOINT).permitAll()
+                                                .anyRequest().authenticated())
+                                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                                .build();
+        }
 
 }
