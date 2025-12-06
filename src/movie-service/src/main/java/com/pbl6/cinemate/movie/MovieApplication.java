0c708defc5c6960c152fc651b92eb24cf1305af1
kuniquoc <102220122@sv1.dut.udn.vn>
2025-12-06 16:07:51 +0700

    feat(security): exclude security auto-configurations in MovieApplication

--- src/movie-service/src/main/java/com/pbl6/cinemate/movie/MovieApplication.java
+++ src/movie-service/src/main/java/com/pbl6/cinemate/movie/MovieApplication.java
@@ -1,14 +1,21 @@
 package com.pbl6.cinemate.movie;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		SecurityAutoConfiguration.class,
+		UserDetailsServiceAutoConfiguration.class,
+		ManagementWebSecurityAutoConfiguration.class
+})
 @EnableScheduling
 @EnableAsync
 @ComponentScan(basePackages = { "com.pbl6.cinemate.movie", "com.pbl6.cinemate.shared" })
