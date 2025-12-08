fb61bc053a1ec490e19887b1fb66b47d44e64dd6
Gia Bao Nguyen <157299978+GiaBaoNguyen7112004@users.noreply.github.com>
2025-12-08 23:11:31 +0700

    fix api favorite (#48)

---
+++ src/customer-service/src/main/java/com/pbl6/microservices/customer_service/client/MovieServiceClient.java
@@ -0,0 +1,26 @@
+package com.pbl6.microservices.customer_service.client;
+
+import com.pbl6.microservices.customer_service.client.dto.MovieServiceResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
+
+/**
+ * Feign client for Movie Service
+ */
+@FeignClient(
+    name = "movie-service",
+    url = "${movie.service.url}"
+)
+public interface MovieServiceClient {
+    
+    /**
+     * Get movie information by ID
+     * Calls: GET /api/movies/{id}
+     * Returns: ResponseData wrapper containing MovieInfoResponse
+     */
+    @GetMapping("/api/movies/{id}")
+    MovieServiceResponse getMovieById(@PathVariable("id") UUID id);
+}
