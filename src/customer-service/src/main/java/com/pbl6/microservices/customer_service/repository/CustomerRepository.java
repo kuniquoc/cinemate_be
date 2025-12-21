46c0bf307114a232496b26f0849388f723bb2739
Gia Bao Nguyen <157299978+GiaBaoNguyen7112004@users.noreply.github.com>
2025-12-21 17:40:40 +0700

    fix: fav (#67)

--- src/customer-service/src/main/java/com/pbl6/microservices/customer_service/repository/CustomerRepository.java
+++ src/customer-service/src/main/java/com/pbl6/microservices/customer_service/repository/CustomerRepository.java
@@ -8,4 +8,6 @@ import java.util.UUID;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
     Optional<Customer> findByAccountId(UUID accountId);
+    
+    boolean existsByAccountId(UUID accountId);
 }
